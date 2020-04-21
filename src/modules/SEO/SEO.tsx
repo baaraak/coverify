59200e620e351b018cd4ec4e0ebca1b53963609b
Danilo Woznica <danilowoz@gmail.com>
2020-04-21 19:02:30 +0100

    feat(seo): adds component

---
+++ src/modules/SEO/SEO.tsx
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import React from 'react'
+
+import faviconSrc from './assets/favicon.png'
+import shareSrc from './assets/share.png'
+import { APP_NAME, DESCRIPTION, SHORT_DESCRIPTION, URL } from 'common/constants'
+
+const SEO = () => {
+  return (
+    <Head>
+      <link rel="shortcut icon" href={faviconSrc} />
+
+      <title>{`${APP_NAME} - ${SHORT_DESCRIPTION}`}</title>
+      <meta name="description" content={DESCRIPTION} />
+      <meta name="image" content={shareSrc} />
+
+      <meta property="og:url" content={URL} />
+      <meta property="og:title" content={APP_NAME} />
+      <meta property="og:description" content={DESCRIPTION} />
+      <meta property="og:image" content={shareSrc} />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={APP_NAME} />
+      <meta name="twitter:description" content={DESCRIPTION} />
+      <meta name="twitter:image" content={shareSrc} />
+    </Head>
+  )
+}
+
+export { SEO }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,13 @@ import Core from 'modules/Core'
 import { TextControl, Cover, Handle } from 'modules/Editor'
 import Header from 'modules/Header'
 import Navigation from 'modules/Navigation'
+import SEO from 'modules/SEO'
 import Stage from 'modules/Stage'
 
 const Index: React.FC = () => {
   return (
     <Core>
+      <SEO />
       <Header />
 
       <Stage>
